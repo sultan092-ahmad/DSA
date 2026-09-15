0d0c562b9ae52a48193b214c6c82584d4158be07
Sultan Ahmad <sultanmuz02@gmail.com>
2026-09-15 22:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (32.59%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int total = n * (n + 1) / 2;
+        for (int num : nums) {
+            total -= num;
+        }
+        return total;
+    }
+}
